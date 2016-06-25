e3dd09bec056f7da8367d1086799755815c3b673
Nadiar AS <codxse@gmail.com>
2016-06-25 15:34:32 +0700

    ikhtisar-statistik router

--- app.js
+++ app.js
@@ -2,6 +2,17 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 3000;
 
+var ikhtisarStatistikRouter = express.Router();
+ikhtisarStatistikRouter.route('/ikhtisar-statistiks')
+  .get(function(req, res) {
+    var responseJson = {
+      hello: "This!"
+    };
+    res.json(responseJson);
+  });
+
+app.use('/v1/', ikhtisarStatistikRouter);
+
 app.get('/', function(req, res) {
   res.send('Batavia RESTful service');
 });
