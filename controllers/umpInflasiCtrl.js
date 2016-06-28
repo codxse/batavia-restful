6f9bfe84138837eaeb6a335abf92971babaffe95
Nadiar AS <codxse@gmail.com>
2016-06-28 18:59:00 +0700

    PATCH verb on umpInflasiCtrl done and working

--- controllers/umpInflasiCtrl.js
+++ controllers/umpInflasiCtrl.js
@@ -39,6 +39,12 @@ exports.createData = function(req, res) {
       res.status(201).send(newUmpInflasiJsonData);
     }
   });
+
+  // add _id to data on Model
+  /*
+   *
+   *
+   */
 };
 
 exports.selectModel = function(req, res) {
@@ -53,6 +59,18 @@ exports.selectModel = function(req, res) {
   });
 };
 
+exports.selectModelById = function(req, res) {
+  Model.findById(req.params._id, function(err, resultJson) {
+    if (err) {
+      res.status(500).send(err);
+    } else if (resultJson) {
+      res.json(resultJson)
+    } else {
+      res.status(404).send('404: data not found')
+    }
+  });
+}
+
 exports.selectData = function(req, res) {
   var query = req.query;
   Data.find(query, function(err, resultJsons) {
@@ -66,4 +84,158 @@ exports.selectData = function(req, res) {
 
 exports.selectDataById = function(req, res) {
   res.json(req.jsonData);
+};
+
+// sort Json Data by Key argumen [asc, desc]
+exports.sortDataByKey = function(req, res) {
+  var query = Data.find();
+  if (req.params._arg === 'desc') {
+    query.sort({
+      [req.params._key]: 'desc'
+    }).exec(function(err, resultJsons) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(resultJsons);
+      }
+    });
+  } else if (req.params._arg === 'asc') {
+    query.sort({
+      [req.params._key]: 'asc'
+    }).exec(function(err, resultJsons) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(resultJsons);
+      }
+    });
+  } else {
+    res.status(404).send('Not Valid Input');
+  }
+};
+
+// get max or min based on key
+exports.getMaxMinData = function(req, res) {
+  var query = Data.findOne();
+  if (req.params._arg === 'max') {
+    query.sort('-'+req.params._key)
+      .exec(function(err, resultJson) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(resultJson);
+        }
+      });
+    } else if (req.params._arg === 'min') {
+      query.sort(req.params._key)
+        .exec(function(err, resultJson) {
+          if (err) {
+            res.status(500).send(err);
+          } else {
+            res.json(resultJson);
+          }
+        });
+    } else {
+      res.status(404).send('Not Valid Input, input in [min, max]');
+    }
+};
+
+// get date from :_ld to :_gd
+exports.getDataBetweenDate = function(req, res) {
+  Data.find({
+    [req.params._key]: {
+      $gt: [req.params._gd],
+      $lt: [req.params._ld]
+    }
+  }).sort({
+    [req.params._key]: 'asc'
+  }).exec(function(err, resultJsons) {
+    if (err) {
+      res.status(500).send(err);
+    } else {
+      res.json(resultJsons);
+    }
+  });
+};
+
+// update All fields from data
+exports.updateAllDataFields = function(req, res) {
+    req.jsonData.tahun = req.body.tahun;
+    req.jsonData.besar_ump = req.body.besar_ump;
+    req.jsonData.persen_kenaikan_ump = req.body.persen_kenaikan_ump;
+    req.jsonData.persen_inflasi = req.body.persen_inflasi;
+    req.jsonData.keterangan = req.body.keterangan;
+    req.jsonData.save(function(err) {
+      if (err) {
+        res.status(500).send(err);
+      } else {
+        res.json(req.jsonData);
+      }
+    });
+};
+
+// update some fields from model
+exports.updateModelFields = function(req, res) {
+  Model.findById(req.params._id, function(err, resultJson) {
+    if (err) {
+      console.log(err);
+      res.status(500).send(err);
+    } else if (resultJson) {
+      req.resultJson = resultJson;
+
+      if (req.body.coefficient) {
+        req.resultJson.model.coefficient = req.body.coefficient;
+      }
+
+      if (req.body.slope) {
+        req.resultJson.model.slope = req.body.slope;
+      }
+
+      if (req.body.data) {
+
+        var isInArray = function (value, array) {
+          return array.indexOf(value) > -1;
+        };
+
+        var arrData = req.resultJson.data;
+
+        // check if data in ArrBody exist in Data collection AND not in arrData
+        // if exist:
+        //    leave
+        // dont?
+        //    push to arrData
+        //    save
+
+        var arrBody = req.body.data;
+        for (index in arrBody) {
+          Data.count({_id: arrBody[index]}, function(err, count) {
+            if (count > 0) {
+              if (!isInArray(arrBody[index], arrData)) {
+                arrData.push(arrBody[index]);
+              }
+            }
+          });
+        }
+
+        // /* Flatering two dimension array */
+        // arrData = arrData.reduce(function(prev, curr) {
+        //   return prev.concat(curr);
+        // });
+
+        req.resultJson.data = arrData;
+      }
+
+      req.resultJson.save(function(err) {
+        if (err) {
+          res.status(500).send(err);
+        } else {
+          res.json(req.resultJson);
+        }
+      });
+
+    // If model id not found
+    } else {
+      res.status(404).send('404: data not found')
+    }
+  });
 };
--- routes/umpInflasiRouter.js
+++ routes/umpInflasiRouter.js
@@ -2,9 +2,10 @@ var express = require('express');
 
 var router = express.Router();
 var umpInflasiCtrl = require('../controllers/umpInflasiCtrl');
+var path = '/ump-inflasi/data';
 
 /* middleware for select ump and inflasi data by id */
-router.use('/ump-inflasi/data/id=:_id', function(req, res, next) {
+router.use(path + '/id=:_id', function(req, res, next) {
   return umpInflasiCtrl.middlewareData(req, res, next);
 });
 
@@ -14,28 +15,53 @@ router.post('/ump-inflasi', function(req, res) {
 });
 
 /* create ump and inflasi data */
-router.post('/ump-inflasi/data', function(req, res) {
+router.post(path, function(req, res) {
   return umpInflasiCtrl.createData(req, res);
 });
 
-/* select linear data */
+/* select linear model */
 router.get('/ump-inflasi', function(req, res) {
   return umpInflasiCtrl.selectModel(req, res);
 });
 
+/* select linear model by id*/
+router.get('/ump-inflasi/id=:_id', function(req, res) {
+  return umpInflasiCtrl.selectModelById(req, res);
+});
+
 /* select ump and inflasi data */
-router.get('/ump-inflasi/data', function(req, res) {
+router.get(path, function(req, res) {
   return umpInflasiCtrl.selectData(req, res);
 });
 
 /* select ump and inflasi data by id */
-router.get('/ump-inflasi/data/id=:_id', function(req, res) {
+router.get(path + '/id=:_id', function(req, res) {
   return umpInflasiCtrl.selectDataById(req, res);
 });
 
 /* sort ump and inflasi data by key */
-router.get('/ump-inflasi/data&sortBy=:_key&order=:_arg', function(req, res) {
+router.get(path + '&sortBy=:_key&order=:_arg', function(req, res) {
   return umpInflasiCtrl.sortDataByKey(req, res);
+});
+
+/* select max min based on key */
+router.get(path + '&key=:_key&get=:_arg', function(req, res) {
+  return umpInflasiCtrl.getMaxMinData(req, res);
+});
+
+/* get data between date */
+router.get(path + '&key=:_key&gd=:_gd&ld=:_ld', function(req, res) {
+  return umpInflasiCtrl.getDataBetweenDate(req, res);
+});
+
+/* update all field for choosen data */
+router.put(path + '/id=:_id', function(req, res) {
+  return umpInflasiCtrl.updateAllDataFields(req, res);
+});
+
+/* update all field for choosen data */
+router.patch('/ump-inflasi/id=:_id', function(req, res) {
+  return umpInflasiCtrl.updateModelFields(req, res);
 });
 
 module.exports = router;
