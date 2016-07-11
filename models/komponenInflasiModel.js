ff4b8bd56c5c61519cfa23939f34980746456375
Nadiar AS <codxse@gmail.com>
2016-07-11 21:50:34 +0700

    hotfix & class diagram

--- models/komponenInflasiModel.js
+++ models/komponenInflasiModel.js
@@ -5,7 +5,7 @@ var dataModel = new Schema({
   tahun: Date,
   komponen: String,
   persen_inflasi: Number,
-  persen_kontribusi: Number,
+  persen_kontribusi: Number
 });
 var data = mongoose.model('KomponenInflasi', dataModel);
 
